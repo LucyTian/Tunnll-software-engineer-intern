e61caeaba26bc0da487c7801da37ab160ee30e67
LucyTian <lut1225@gmail.com>
2017-07-04 16:07:34 -0400

    Tried two different input

--- learnPythonHD/website/gothonweb/bin/app.py
+++ learnPythonHD/website/gothonweb/bin/app.py
@@ -7,11 +7,14 @@ app = web.application(urls, globals())
 # the font and size is under the template directory
 render = web.template.render('templates/')
 class Index(object):
-    form = web.input(name="Nobody")
     def GET(self):
-        greeting = "Hello, %s" % form,name
-        #return render.index(greeting)
-        return render.index(greeting = greeting)
-
+        form = web.input(name="Nobody",greet = None)
+        if form.greet:
+            greeting = "%s, %s" % (form.greet,form.name)
+            #http://localhost:8080/hello?name=Frank&greet=Hola This url would work
+            #return render.index(greeting)
+            return render.index(greeting = greeting)
+        else:
+            return "ERROR: greet is required."
 if __name__ == "__main__":
     app.run()
