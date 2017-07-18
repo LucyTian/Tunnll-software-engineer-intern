6a89891c9eff8d0ffa152f5101e62a3f060df7cd
LucyTian <lut1225@gmail.com>
2017-07-17 23:38:55 -0400

    Add a map using google map API

--- MyMap/app/src/main/java/com/lu/mymap/MainActivity.kt
+++ MyMap/app/src/main/java/com/lu/mymap/MainActivity.kt
@@ -2,22 +2,40 @@ package com.lu.mymap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.view.View
-import android.view.View.OnClickListener
+import android.support.v4.app.Fragment;
+
 import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+
+
+
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btn1:ImageButton = findViewById(R.id.btn1)
-        btn1.setOnClickListener {
-            // TODO Auto-generated method stub
+        val btnExit: ImageButton = findViewById(R.id.btn1)
+
+        btnExit.setOnClickListener {
             finish()
             System.exit(0)
         }
+
+        val mapFragment:SupportMapFragment = supportFragmentManager.
+                findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney: LatLng = LatLng(-33.852,151.211)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("This is Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
